7f4cc1e0cb6c7ac565af3de0b145107936e88955
Anthony <anthonyjosue0011@gmail.com>
2025-07-22 13:39:40 -0500

    fix: habilita CORS para Vercel y ajustes de conexión en producción

--- src/main.ts
+++ src/main.ts
@@ -6,8 +6,12 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: ['http://localhost:3000', 'http://localhost:3001'],
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    origin: [
+      'http://localhost:3000', 
+      'http://localhost:3001',
+      'https://proyecto-final-frontend-two.vercel.app'],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
   });
 
